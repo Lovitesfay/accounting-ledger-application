d0cb1343e243192a4509147f931434788bd4d725
lovitesfay <lovitesfay@gmail.com>
2026-04-28 09:35:02 -0500

    added a class and methods

--- src/main/java/AccountingLedgerApp.java
+++ src/main/java/AccountingLedgerApp.java
@@ -1,53 +1,83 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AccountingLedgerApp {
     public static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.print("""
-                
-                What do you want to do?
-                     1.Home
-                     2 Ledger
-                 Make you selection:
-                """);
+        ArrayList<Transaction> transactions = new ArrayList<>();
+        {
+            System.out.print("""
+                    
+                    What do you want to do?
+                         1. Home
+                         2. Ledger
+                         0. Exit
+                     Make you selection:
+                    """);
 
-        int mainScreen = input.nextInt();
+            int mainScreen = input.nextInt();
 
-        switch (mainScreen) {
-            case 1:
-                HomePage();
-                break;
+            switch (mainScreen) {
+                case 1:
+                    homePage(transactions);
+                    break;
 
-            case 2:
-                Ledger();
-                break;
+                case 2:
+                    ledger(transactions);
+                    break;
+                case 3:
+                    System.exit(0);
 
+            }
         }
-
     }
 
-    public static void HomePage() {
-        System.out.println("""
-                ______Home Screen_______
+    public static void homePage(ArrayList<Transaction> transactions) {
+        System.out.print("""
+                Welcome to the HomeScreen
                 
                 Choose from the list:
                 
                 ▪ Add Deposit - (D)
                 ▪ P) Make Payment
                 ▪ L) Ledger
+                  M) Main Menu
                 ▪ X) Exit
                 """);
 
 
-        int homepage = input.nextInt();
+        String choice = input.nextLine().toUpperCase();
+
+        switch (choice){
+            case "D":
+                addDeposit(transactions);
+                break;
+            case "P":
+                break;
+            case "L":
+                ledger(transactions);
+
+
+
+
+        }
 
 
     }
 
-    public static void Ledger() {
+    public static void ledger(ArrayList<Transaction>transactions) {
 
         int ledger = input.nextInt();
+
+    }
+    public static void loadTransaction(ArrayList<Transaction> transactions){
+
+    }
+    public static void addDeposit(ArrayList<Transaction> transactions){
+
+    }
+    public static void saveTransaction(ArrayList<Transaction> transactions){
 
     }
 }
---
+++ src/main/java/Transaction.java
@@ -0,0 +1,38 @@
+public class Transaction {
+    private String date;
+    private String description;
+    private double amount;
+
+    public Transaction(String date, String description, double amount) {
+        this.date = date;
+        this.description = description;
+        this.amount = amount;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public Transaction setDate(String date) {
+        this.date = date;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Transaction setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public Transaction setAmount(double amount) {
+        this.amount = amount;
+        return this;
+    }
+}
