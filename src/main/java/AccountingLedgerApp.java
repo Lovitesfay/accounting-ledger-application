1af1b47e3fd0d430d9c39883921991ccdfdea7db
lovitesfay <lovitesfay@gmail.com>
2026-04-29 13:55:44 -0500

    added the ledger methods

--- src/main/java/AccountingLedgerApp.java
+++ src/main/java/AccountingLedgerApp.java
@@ -7,11 +7,12 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class AccountingLedgerApp {
+
     public static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
         ArrayList<Transaction> transactions = new ArrayList<>();
-
+        readTransaction(transactions);
         while (true) {
             {
                 System.out.print("""
@@ -27,10 +28,10 @@ public class AccountingLedgerApp {
 
                 switch (mainScreen) {
                     case "1":
-                        homePage(transactions);
+                        homeScreen(transactions);
                         break;
                     case "2":
-                        ledger(transactions);
+                        ledgerScreen(transactions);
                         break;
                     case "0":
                         System.exit(0);
@@ -42,7 +43,8 @@ public class AccountingLedgerApp {
         }
     }
 
-    public static void homePage(ArrayList<Transaction> transactions) throws IOException {
+    public static void homeScreen(ArrayList<Transaction> transactions)
+            throws IOException {
         while (true) {
             System.out.print("""
                     
@@ -64,7 +66,7 @@ public class AccountingLedgerApp {
                     makePayment(transactions);
                     break;
                 case "L":
-                    ledger(transactions);
+                    ledgerScreen(transactions);
                     break;
                 case "X":
                     return;
@@ -77,7 +79,8 @@ public class AccountingLedgerApp {
         }
     }
 
-    public static void addDeposit(ArrayList<Transaction> transactions) throws IOException {
+    public static void addDeposit(ArrayList<Transaction> transactions)
+            throws IOException {
         System.out.println("Please fill in the blank");
 
         System.out.print("Enter date: ");
@@ -96,14 +99,15 @@ public class AccountingLedgerApp {
         Transaction transact = new Transaction(date, time, describe, vendor, amount);
 
         transactions.add(transact);
-        saveTransaction(transact);
+        writeTransaction(transact);
 
         System.out.println("We've received your Deposit!");
 
 
     }
 
-    public static void makePayment(ArrayList<Transaction> transactions) throws IOException {
+    public static void makePayment(ArrayList<Transaction> transactions)
+            throws IOException {
 
         System.out.println("Please fill in the blank");
         System.out.println(" ");
@@ -123,99 +127,240 @@ public class AccountingLedgerApp {
         input.nextLine();
         amount = -Math.abs(amount);
         System.out.println(" ");
-        Transaction transact = new Transaction(date, time, describe ,vendor, amount);
+        Transaction transact = new Transaction(date, time, describe, vendor, amount);
 
         transactions.add(transact);
-        saveTransaction(transact);
+        writeTransaction(transact);
 
         System.out.println("We've received your Payment !");
     }
 
-    public static void ledger(ArrayList<Transaction> transactions) {
+    public static void ledgerScreen(ArrayList<Transaction> transactions) {
         System.out.println("""
                 Ledger - All entries should show the newest entries first
                 o A) All
                 o D) Deposits
                 o P) Payments
                 o R) Reports
+                """);
+
+        String choice = input.nextLine().toUpperCase();
+
+        switch (choice) {
+            case "A":
+                listAll(transactions);
+                break;
+            case "D":
+                listDeposits(transactions);
+                break;
+            case "P":
+                listPayments(transactions);
+                break;
+            case "R":
+                listReports(transactions);
+                return;
+            default:
+                System.out.println("Invalid option. Please try again.");
+
+
+        }
+
+
+        String ledger = input.nextLine();
+    }
+
+    public static void listAll(ArrayList<Transaction> transactions) {
+        System.out.println("Here you can see everything");
+        System.out.println("\n====== ALL TRANSACTIONS ======\n");
+
+        for (int i = transactions.size() - 1; i >= 0; i--) {
+            Transaction t = transactions.get(i);
+
+            System.out.println(
+                    t.getDate() + " | " +
+                            t.getTime() + " | " +
+                            t.getDescription() + " | " +
+                            t.getVendor() + " | " +
+                            t.getAmount()
+            );
+        }
+    }
+
+    public static void listDeposits(ArrayList<Transaction> transactions) {
+        System.out.println("List of the Deposits made");
+        System.out.println(" ");
+        System.out.println("\n====== DEPOSITS ======\n");
+
+        for (int i = transactions.size() - 1; i >= 0; i--) {
+            Transaction transaction = transactions.get(i);
+
+            if (transaction.getAmount() > 0) {
+                System.out.println(
+                        transaction.getDate() + " | " +
+                        transaction.getTime() + " | " +
+                        transaction.getDescription() + " | " +
+                        transaction.getVendor() + " | " +
+                        transaction.getAmount()
+                );
+            }
+        }
+    }
+
+    public static void listPayments(ArrayList<Transaction>transactions) {
+        System.out.println("\n====== PAYMENTS ======\n");
+
+        for (int j = transactions.size() - 1; j >= 0; j--) {
+            Transaction transaction = transactions.get(j);
+
+            if (transaction.getAmount() < 0) {
+                System.out.println(
+                        transaction.getDate() + " | " +
+                                transaction.getTime() + " | " +
+                                transaction.getDescription() + " | " +
+                                transaction.getVendor() + " | " +
+                                transaction.getAmount()
+                );
+            }
+        }
+
+    }
+
+    public static void listReports(ArrayList<Transaction>transactions) {
+
+        System.out.println("List of the Reports");
+
+        System.out.println("""
+                R) Reports
                 ▪ 1) Month To Date
                 ▪ 2) Previous Month
                 ▪ 3) Year To Date
                 ▪ 4) Previous Year
                 ▪ 5) Search by Vendor
-                ▪ 0) Back - go back to the Ledger page
-                o H) Home - go back to the home page
-                
+                ▪ 0) Back
                 """);
 
-        String ledger = input.nextLine();
+        String choice = input.nextLine().toUpperCase();
+
+        switch (choice) {
+            case "1":
+            monthToDate(transactions);
+                break;
+            case "2":
+            previousMonth(transactions);
+                break;
+            case "3":
+            yearToDate(transactions);
+                break;
+            case "4":
+            previousYear(transactions);
+            case "5":
+
+                break;
+            case "0":
+                return;
+            default:
+                System.out.println("Invalid option. Please try again.");
+
+
+        }
+
+
+
+
+
     }
 
-    public static void listAll (ArrayList<Transaction> transactions){
-        System.out.println("Here you can see everything");
+    public static void monthToDate(ArrayList<Transaction>transactions) {
 
-    }
+        System.out.println("\n--- MONTH TO DATE ---");
 
-    public static void listDeposits (ArrayList<Transaction> transactions){
-        System.out.println("List of the Deposits made");
-        System.out.println(" ");
-        System.out.println("\n====== DEPOSITS ======\n");
+        for (int i = transactions.size() - 1; i >= 0; i--) {
+            Transaction t = transactions.get(i);
 
-        for(int i = transactions.size()-1; i > 0; i --)  {
-            Transaction transaction = transactions.get(i);
-
-            if (transaction.getAmount() > 0 ) {
-                System.out.println(transaction.getDate() + transaction.getTime() +
-                        transaction.getDescription() + transaction.getVendor() +
-                        transaction.getAmount());
+            if (t.getDate().startsWith("04/")) { // example: April
+                for (Transaction transaction : transactions) {
+                    System.out.println(
+                            transaction.getDate() + " | " +
+                            transaction.getTime() + " | " +
+                            transaction.getDescription() + " | " +
+                            transaction.getVendor() + " | " +
+                            transaction.getAmount());
+                }
             }
         }
     }
 
-    public static void listPayments (ArrayList<Transaction> transactions) {
-        System.out.println("\n====== DEPOSITS ======\n");
+    public static void previousMonth(ArrayList<Transaction>transactions) {
+        System.out.println("\n--- PREVIOUS MONTH ---");
 
-        for(int i = transactions.size()-1; i > 0; i --)  {
-            Transaction transaction = transactions.get(i);
+        for (Transaction transaction : transactions) {
+            System.out.println(
+                    transaction.getDate() + " | " +
+                            transaction.getTime() + " | " +
+                            transaction.getDescription() + " | " +
+                            transaction.getVendor() + " | " +
+                            transaction.getAmount());
+        }
+    }
 
-            if (transaction.getAmount() > 0 ) {
-                System.out.println(transaction.getDate() + transaction.getTime() +
-                        transaction.getDescription() + transaction.getVendor() +
+    public static void yearToDate(ArrayList<Transaction>transactions) {
+        System.out.println("\n--- YEAR TO DATE ---");
+
+        for (Transaction transaction : transactions) {
+            System.out.println(
+                    transaction.getDate() + " | " +
+                    transaction.getTime() + " | " +
+                    transaction.getDescription() + " | " +
+                    transaction.getVendor() + " | " +
+                    transaction.getAmount());
+        }
+    }
+
+    public static void previousYear(ArrayList<Transaction>transactions){
+
+        System.out.println("\n--- PREVIOUS YEAR ---");
+
+        for (Transaction transaction : transactions) {
+            System.out.println(
+                    transaction.getDate() + " | " +
+                    transaction.getTime() + " | " +
+                    transaction.getDescription() + " | " +
+                    transaction.getVendor() + " | " +
+                    transaction.getAmount());
+        }
+    }
+
+    public static void searchByVendor(ArrayList<Transaction>transactions){
+
+    }
+
+    public static void readTransaction(ArrayList<Transaction> transactions) {
+
+    }
+
+    public static void writeTransaction(Transaction transaction)
+            throws IOException {
+        try (BufferedWriter writeBuffer = new BufferedWriter(new FileWriter
+                ("src/main/resources/transactions.csv", true))) {
+
+
+            writeBuffer.write(
+                    transaction.getDate() + "|" +
+                        transaction.getTime() + "|" +
+                        transaction.getDescription() + "|" +
+                        transaction.getVendor() + "|" +
                         transaction.getAmount());
-            }
+
+
+            writeBuffer.newLine();
+
+
+        } catch (IOException e) {
+            System.out.println("pls try again" + e.getMessage());
+
+
+
         }
 
     }
-
-    public static void listReports (ArrayList<Transaction> transactions){
-        System.out.println("List of the Reports");
-    }
-
-    public static void loadTransaction(ArrayList<Transaction> transactions) {
-
-    }
-
-    public static void saveTransaction(Transaction transaction) throws IOException {
-       try (BufferedWriter writeBuffer = new BufferedWriter(new FileWriter
-               ("src/main/resources/transactions.csv",true))){
-
-           writeBuffer.write("date|time|description|vendor|amount|");
-           writeBuffer.newLine();
-           writeBuffer.write(
-                   transaction.getDate() + "|" +
-                           transaction.getTime() + "|" +
-                           transaction.getDescription() + "|" +
-                           transaction.getVendor() + "|" +
-                           transaction.getAmount());
-
-
-           writeBuffer.newLine();
-
-
-       } catch (IOException e) {
-           System.out.println("pls try again" + e.getMessage());
-
-       }
-
 }
-    }
