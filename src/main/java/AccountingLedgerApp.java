fd6db8cebe3155a0aa6860a4b590893a21967e85
lovitesfay <lovitesfay@gmail.com>
2026-04-28 20:14:53 -0500

    created the csv file

--- src/main/java/AccountingLedgerApp.java
+++ src/main/java/AccountingLedgerApp.java
@@ -1,10 +1,13 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AccountingLedgerApp {
     public static Scanner input = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ArrayList<Transaction> transactions = new ArrayList<>();
 
         while (true) {
@@ -36,51 +39,55 @@ public class AccountingLedgerApp {
         }
     }
 
-    public static void homePage(ArrayList<Transaction> transactions) {
-       while(true) {
-           System.out.print("""
-                  
-                   ▪ Add Deposit - (D)
-                   ▪ P) Make Payment
-                   ▪ L) Ledger
-                   ▪ X) Exit
-                   
-                   What would you like to do today?
-                   """);
+    public static void homePage(ArrayList<Transaction> transactions) throws IOException {
+        while (true) {
+            System.out.print("""
+                    
+                    ▪ Add Deposit - (D)
+                    ▪ P) Make Payment
+                    ▪ L) Ledger
+                    ▪ X) Exit
+                    
+                    What would you like to do today?
+                    """);
 
-           String choice = input.nextLine().toUpperCase();
+            String choice = input.nextLine().toUpperCase();
 
-           switch (choice) {
-               case "D":
-                   addDeposit(transactions);
-                   break;
-               case "P":
-                   makePayment(transactions);
-                   break;
-               case "L":
-                   ledger(transactions);
-                   break;
-               case "X":
-                   return;
+            switch (choice) {
+                case "D":
+                    addDeposit(transactions);
+                    break;
+                case "P":
+                    makePayment(transactions);
+                    break;
+                case "L":
+                    ledger(transactions);
+                    break;
+                case "X":
+                    return;
 
 
-           }
+            }
 
-       }
+        }
     }
 
-    public static void addDeposit(ArrayList<Transaction> transactions){
+    public static void addDeposit(ArrayList<Transaction> transactions) throws IOException {
         System.out.println("Please fill in the blank");
 
         System.out.print("Enter date: ");
         String date = input.nextLine();
+        System.out.println("Enter Time: ");
+        String time = input.nextLine();
         System.out.print("Enter Description: ");
         String describe = input.nextLine();
+        System.out.println("Vendor Name: ");
+        String vendor = input.nextLine();
         System.out.print("Enter amount: ");
         double amount = input.nextDouble();
         input.nextLine();
         System.out.println(" ");
-        Transaction transact = new Transaction(date, describe, amount);
+        Transaction transact = new Transaction(date, time, amount);
 
         transactions.add(transact);
         saveTransaction(transact);
@@ -90,15 +97,21 @@ public class AccountingLedgerApp {
 
     }
 
-    public static void makePayment(ArrayList<Transaction> transactions){
+    public static void makePayment(ArrayList<Transaction> transactions) throws IOException {
 
         System.out.println("Please fill in the blank");
-        System.out.println("[Make a Deposit]");
+        System.out.println(" ");
+        System.out.println("[Make a Payment]");
+        System.out.println(" ");
 
         System.out.print("Enter date: ");
         String date = input.nextLine();
+        System.out.println("Enter Time: ");
+        String time = input.nextLine();
         System.out.print("Enter Description: ");
         String describe = input.nextLine();
+        System.out.println("Vendor Name: ");
+        String vendor = input.nextLine();
         System.out.print("Enter amount: ");
         double amount = input.nextDouble();
         input.nextLine();
@@ -112,34 +125,50 @@ public class AccountingLedgerApp {
         System.out.println("We've received your Payment !");
     }
 
-    public static void ledger(ArrayList<Transaction>transactions) {
+    public static void ledger(ArrayList<Transaction> transactions) {
         System.out.println("""
                 Ledger - All entries should show the newest entries first
-                o A) All - Display all entries
-                o D) Deposits - Display only the entries that are deposits into the account
-                o P) Payments - Display only the negative entries (or payments)
-                o R) Reports - A new screen that allows the user to run pre-defined reports or
-                to run a custom search
+                o A) All
+                o D) Deposits
+                o P) Payments
+                o R) Reports
                 ▪ 1) Month To Date
                 ▪ 2) Previous Month
                 ▪ 3) Year To Date
                 ▪ 4) Previous Year
-                ▪ 5) Search by Vendor - prompt the user for the vendor name and
-                display all entries for that vendor
+                ▪ 5) Search by Vendor
                 ▪ 0) Back - go back to the Ledger page
                 o H) Home - go back to the home page
                 
                 """);
 
-        int ledger = input.nextInt();
-        input.nextLine();
+        String ledger = input.nextLine();
     }
 
-    public static void loadTransaction(ArrayList<Transaction> transactions){
+    public static void loadTransaction(ArrayList<Transaction> transactions) {
 
     }
 
-    public static void saveTransaction(Transaction transaction){
+    public static void saveTransaction(Transaction transaction) throws IOException {
+       try (BufferedWriter writeBuffer = new BufferedWriter(new FileWriter
+               ("src/main/resources/transactions.csv"))){
+           writeBuffer.write("");
+           writeBuffer.newLine();
+           writeBuffer.write("                           Transactions                                ");
+           writeBuffer.newLine();
+           writeBuffer.write("______________________________________________________________________");
+           writeBuffer.newLine();
+           writeBuffer.write(" ");
+           writeBuffer.newLine();
+           writeBuffer.write("date|description|amount");
+           writeBuffer.newLine();
+           writeBuffer.write(transaction.getDate()+  "|" +
+                   transaction.getDescription() + "|" +
+                   transaction.getAmount());
 
-    }
+
+           writeBuffer.close();
+       }
+
 }
+    }
--- src/main/java/Transaction.java
+++ src/main/java/Transaction.java
@@ -1,38 +1,58 @@
 public class Transaction {
     private String date;
+    private String time;
     private String description;
+    private String vendor;
     private double amount;
 
-    public Transaction(String date, String description, double amount) {
+    public Transaction(String date, String time, String description, String vendor, double amount) {
         this.date = date;
+        this.time = time;
         this.description = description;
+        this.vendor = vendor;
         this.amount = amount;
+    }
+
+    public Transaction(String date, String describe, double amount) {
     }
 
     public String getDate() {
         return date;
     }
 
-    public Transaction setDate(String date) {
+    public void setDate(String date) {
         this.date = date;
-        return this;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
     }
 
     public String getDescription() {
         return description;
     }
 
-    public Transaction setDescription(String description) {
+    public void setDescription(String description) {
         this.description = description;
-        return this;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
     }
 
     public double getAmount() {
         return amount;
     }
 
-    public Transaction setAmount(double amount) {
+    public void setAmount(double amount) {
         this.amount = amount;
-        return this;
     }
 }
